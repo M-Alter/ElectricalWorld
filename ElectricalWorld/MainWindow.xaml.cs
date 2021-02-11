db5a826316d28d439d1959ecdd9e3a0869bf13b6
Menachem <alter.menachem@gmail.com>
2021-02-11 17:49:29 +0200

    search

--- ElectricalWorld/MainWindow.xaml.cs
+++ ElectricalWorld/MainWindow.xaml.cs
@@ -1,19 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using BL;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using BL;
 
 namespace ElectricalWorld
 {
@@ -25,17 +13,18 @@ namespace ElectricalWorld
         IBL bl = BLAPI.BLFactory.GetBL();
         ObservableCollection<BO.Item> items = new ObservableCollection<BO.Item>();
         ObservableCollection<BO.Order> sales = new ObservableCollection<BO.Order>();
+        ObservableCollection<BO.Customer> custs = new ObservableCollection<BO.Customer>();
 
 
         public MainWindow()
         {
             InitializeComponent();
 
-            foreach (var item in bl.GetItems(it => true))
-            {
-                items.Add(item);
-            }
-            lvItems.DataContext = items;
+            //foreach (var item in bl.GetItems(it => true))
+            //{
+            //    items.Add(item);
+            //}
+            //lvItems.DataContext = items;
 
         }
 
@@ -57,23 +46,61 @@ namespace ElectricalWorld
 
         private void btnSalesSearch_Click(object sender, RoutedEventArgs e)
         {
+            sales.Clear();
 
-            foreach (var item in bl.GetOrders(
-                ord => ord.Customer.CustomerID.ToString() == tboxSalesSearch.Text ||
-                ord.Customer.FirstName.Contains(tboxSalesSearch.Text) ||
-                ord.Customer.LastName.Contains(tboxSalesSearch.Text) ||
-                ord.Customer.Phone.Contains(tboxSalesSearch.Text) ||
-                ord.Customer.Mobile.Contains(tboxSalesSearch.Text) ||
-                ord.Customer.PostCode.Contains(tboxSalesSearch.Text) ||
-                ord.Customer.Address.Contains(tboxSalesSearch.Text) ||
-                ord.Customer.Email.Contains(tboxSalesSearch.Text) ||
-                ord.OrderID.ToString().StartsWith(tboxSalesSearch.Text)
+            foreach (var item in bl.GetOrders(ord =>
+                ord.Customer.CustomerID.ToString().ToLower() == tboxSalesSearch.Text.ToLower() ||
+                ord.Customer.FirstName.ToLower().Contains(tboxSalesSearch.Text.ToLower()) ||
+                ord.Customer.LastName.ToLower().Contains(tboxSalesSearch.Text.ToLower()) ||
+                ord.Customer.Phone.ToLower().Contains(tboxSalesSearch.Text.ToLower()) ||
+                ord.Customer.Mobile.ToLower().Contains(tboxSalesSearch.Text.ToLower()) ||
+                ord.Customer.PostCode.ToLower().Contains(tboxSalesSearch.Text.ToLower()) ||
+                ord.Customer.Address.ToLower().Contains(tboxSalesSearch.Text.ToLower()) ||
+                ord.Customer.Email.ToLower().Contains(tboxSalesSearch.Text.ToLower()) ||
+                ord.OrderID.ToString().ToLower().StartsWith(tboxSalesSearch.Text.ToLower())
                  )
                 )
             {
                 sales.Add(item);
             }
             lvSales.DataContext = sales;
+        }
+
+        private void btnCustSearch_Click(object sender, RoutedEventArgs e)
+        {
+            custs.Clear();
+
+            foreach (var item in bl.GetCutomers(cust =>
+                cust.CustomerID.ToString().ToLower().Contains(tboxCustSearch.Text.ToLower()) ||
+                cust.FirstName.ToLower().Contains(tboxCustSearch.Text.ToLower()) ||
+                cust.LastName.ToLower().Contains(tboxCustSearch.Text.ToLower()) ||
+                cust.Phone.ToLower().Contains(tboxCustSearch.Text.ToLower()) ||
+                cust.Mobile.ToLower().Contains(tboxCustSearch.Text.ToLower()) ||
+                cust.Address.ToLower().Contains(tboxCustSearch.Text.ToLower()) ||
+                cust.PostCode.ToLower().Contains(tboxCustSearch.Text.ToLower()) ||
+                cust.Email.ToLower().Contains(tboxCustSearch.Text.ToLower())
+                )
+                )
+            {
+                custs.Add(item);
+            }
+            lvCusts.DataContext = custs;
+        }
+
+        private void btnProdSearch_Click(object sender, RoutedEventArgs e)
+        {
+            items.Clear();
+
+            foreach (var item in bl.GetItems(it =>
+                it.ItemID.ToString().ToLower().Contains(tboxProdSearch.Text.ToLower()) ||
+                it.Brand.ToLower().Contains(tboxProdSearch.Text.ToLower()) ||
+                it.ModelNumber.ToLower().Contains(tboxProdSearch.Text.ToLower().ToLower())
+                )
+                )
+            {
+                items.Add(item);
+            }
+            lvItems.DataContext = items;
         }
     }
 }
